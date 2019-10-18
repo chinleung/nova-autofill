f8b49c6334ea1b8b774f35a78ccda7857f848c33
Chin Leung <hello@chinleung.com>
2019-10-17 20:33:13 -0400

    Initial commit

---
+++ src/NovaAutofillServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ChinLeung\LaravelNovaAutofill;
+
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class NovaAutofillServiceProvider
+{
+    /**
+     * Boostrap the application by adding a macro to the fields.
+     *
+     * @return void
+     */
+    public function boot() : void
+    {
+        Field::macro(
+            'autofill',
+            function (string $attribute = null) {
+                $request = app(NovaRequest::class);
+
+                $shouldAutofill = $request->isCreateOrAttachRequest()
+                    && ($instance = $request->findParentModel());
+
+                if ($shouldAutofill) {
+                    $this->withMeta([
+                        'value' => $instance->{$attribute ?? $this->attribute},
+                    ]);
+                }
+
+                return $this;
+            }
+        );
+    }
+}
