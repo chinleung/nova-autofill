64b4aef7994d670ba335615237b72a03441aa99e
Chin Leung <hello@chinleung.com>
2021-11-10 09:52:48 -0500

    Added support for nested relations attributes

--- src/NovaAutofillServiceProvider.php
+++ src/NovaAutofillServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace ChinLeung\NovaAutofill;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -13,24 +14,24 @@ class NovaAutofillServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot() : void
+    public function boot(): void
     {
-        Field::macro(
-            'autofill',
-            function (string $attribute = null) {
-                $request = app(NovaRequest::class);
+        Field::macro('autofill', function (string $attribute = null) {
+            $request = app(NovaRequest::class);
 
-                $shouldAutofill = $request->isCreateOrAttachRequest()
-                    && ($instance = $request->findParentModel());
+            $shouldAutofill = $request->isCreateOrAttachRequest()
+                && ($instance = $request->findParentModel());
 
-                if ($shouldAutofill) {
-                    $this->withMeta([
-                        'value' => $instance->{$attribute ?? $this->attribute},
-                    ]);
-                }
-
-                return $this;
+            if ($shouldAutofill) {
+                $this->withMeta([
+                    'value' => Arr::get(
+                        $instance,
+                        $attribute ?? $this->attribute
+                    ),
+                ]);
             }
-        );
+
+            return $this;
+        });
     }
 }
